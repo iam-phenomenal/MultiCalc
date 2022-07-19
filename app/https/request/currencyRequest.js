20b98e56411b2508c80780b532146362ec4103f6
4ti5ied <ayandiranibrahim2015@gmail.com>
2022-07-19 04:06:17 +0100

    currency converter Added

--- app/https/request/currencyRequest.js
+++ app/https/request/currencyRequest.js
@@ -1,7 +1,8 @@
-const currencyController = require("../controller/currencyController")
 const {validationResult} = require("express-validator")
+const CC = require("currency-converter-lt")
 
 const currencyRequestHandler = async (req, res) =>{
+    var result = ""
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(422).json({errors: errors.array()})
@@ -11,9 +12,14 @@ const currencyRequestHandler = async (req, res) =>{
     var destCurrency = req.body.destCurrency
 
     try{
-        var result,  rate = currencyController(amount, baseCurrency, destCurrency)
-        res.status(200).json(result, rate)
-    
+        var currencyConverter = new CC()
+        currencyConverter.from(baseCurrency).to(destCurrency).amount(amount).convert().then((response)=>{
+            res.status(200).json({
+                fromCurrency: `${baseCurrency} ${amount}`,
+                toCurrency: `${destCurrency} ${response}`
+            })
+        })
+
     }catch(err){
         res.status(500).json(err.message)
     }
--- routes/currencyRoute.js
+++ routes/currencyRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const currencyRequestHandler = require("../app/https/request/currencyRequest")
 const validator = require("../app/providers/validators/currencyValidator")
-
+const CC = require("currency-converter-lt")
 
 router.post("", validator(), currencyRequestHandler)
 
